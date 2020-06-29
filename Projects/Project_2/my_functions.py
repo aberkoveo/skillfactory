dc5f52a929d61a491e3dbc8a15d499d875d5df41
aberkoveo <aberkoveo@gmail.com>
2020-06-29 23:56:48 +0300

    commit-to-save

--- Projects/Project_2/my_functions.py
+++ Projects/Project_2/my_functions.py
@@ -1,10 +1,5 @@
 import pandas as pd
 
-def to_binary(series):
-    series = series
-
-
+def yn_to_binary(series):
+    series = series.map(lambda series: 1 if series == 'yes' else 0 if series == 'no' else series)
     return series
-
-
-data = pd.read_csv('stud_math.csv')
