abe4d6a58d62ba0bdf9dd12a3cec8367e0a86e5d
aberkoveo <aberkoveo@gmail.com>
2020-08-15 16:25:16 +0300

    func+

--- Projects/Project_3/my_functions.py
+++ Projects/Project_3/my_functions.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from itertools import combinations
 from scipy.stats import ttest_ind
-
+from datetime import datetime
 
 def get_boxplot(data, column, target):
     """
@@ -27,3 +27,36 @@ def get_boxplot(data, column, target):
     plt.ylabel(target, fontsize=20)
     ax.set_title('Boxplot for ' + column, fontsize=23)
     plt.show()
+
+
+def review_text_tone(review):
+    text_tone_coef = 0
+
+    good_words = set(['lovely','very good', 'excellent', 'best', 'nice', 'great','beautifull',
+                      'awesome', 'awsom', 'yummy', 'friendly', 'not bad', 'well', 'tasty',
+                      'good experience', 'perfect', 'wonderful', 'pleasant', 'helpful', 'cosy',
+                      'healthy', 'cute', 'fantastic'])
+
+    bad_words = set(['what the heck', 'waste', 'really bad', 'please try another', 
+                     'avoid at all cost', 'terrible', 'worst', 'Not good', 'bad service',
+                     'dirty', 'shabby', 'confused', 'disappointing', 'rude', 'no go', 
+                     'slow service', 'awful', 'disgusting'])
+    
+    for word in good_words:
+        for rev in review:
+            if word in rev.lower():
+                text_tone_coef += 0.5
+
+    for word in bad_words:
+        for rev in review:
+            if word in rev.lower():
+                text_tone_coef -= 0.5
+
+    return text_tone_coef
+
+
+def diff_today(date_list):
+    dif_today = 0
+    if len(date_list) > 0:
+        dif_today = (datetime.today().date() - max(date_list)).days
+    return dif_today
