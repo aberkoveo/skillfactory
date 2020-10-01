dab604e7808dcea21daebca49c5682a782c3e713
aberkoveo <aberkoveo@gmail.com>
2020-10-02 02:19:24 +0300

    fillna-abit-analys

---
+++ Projects/Project_4/my_func.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from itertools import combinations
+from scipy.stats import ttest_ind
+from datetime import datetime
+
+from collections import Counter
+from datetime import datetime
+import re
+import ast
+from scipy import stats
+
+
+
+def get_boxplot(data, column, target):
+    """
+    функция строит график типа seaborn.boxplot на основе введенных параметров.
+    Принимает объекты dataframe, имя колонки для оси Х имя колонки для оси Y
+    """
+    fig, ax = plt.subplots(figsize = (20, 5))
+    sns.boxplot(x=column, y=target, 
+                data=data,
+               ax=ax)
+    plt.xticks(rotation=45)
+    plt.xlabel(column, fontsize=20)
+    plt.ylabel(target, fontsize=20)
+    ax.set_title('Boxplot for ' + column, fontsize=23)
+    plt.show()
