f7193966ab2f7cb63cffd176f722ccce751a88e2
aberkoveo <aberkoveo@gmail.com>
2020-10-03 04:02:58 +0300

    added_out_class

--- Projects/Project_4/main.py
+++ Projects/Project_4/main.py
@@ -23,52 +23,65 @@ class df_scrolling_object():
         # input_folder = 'input/'
         self.data = pd.read_csv('input/train.csv')
         self.data.drop(['client_id'],  axis=1, inplace=True)
-        # data = data.copy()
+        data = self.data.copy()
         self.bin_cols = ['good_work', 'foreign_passport', 'car', 'car_type', 'sex']
         self.num_cols = ['age', 'decline_app_cnt', 'bki_request_cnt', 'income']
         self.cat_cols = ['education', 'home_address' , 'work_address']
 
-        edu_income_rel = dict({'SCH': list([0, 31960.275039]),
+        self.edu_income_rel = dict({'SCH': list([0, 31960.275039]),
                                     'UGR': list([31960.275039,39391.796251]),
                                     'GRD': list([39391.796251, 54663.948941]),
                                     'PGR': list([54663.948941, 77548.206046]),
                                     'ACD': list([77548.206046, 999999999999])})
 
-        na_index = self.data[self.data.education.isna()].index
-
+        self.data['education_isNAN'] = pd.isna(self.data.education).astype('uint8')
+        na_index = self.data[self.data.education.isna()].index                                    
         for index in na_index:
             inc = self.data.income.iloc[index]
-            self.data.education.iloc[index] = self.fill_edu(inc, edu_income_rel)
-        
-        self.data['education_isNAN'] = pd.isna(self.data.education).astype('uint8')
+            self.data.education.iloc[index] = self.fill_edu(inc, self.edu_income_rel)
 
-        for num_col in self.num_cols:
-            self.data[num_col] = self.data[num_col].apply(lambda x: np.log(x + 1))
 
-        label_encoder = LabelEncoder()
-        for bin_col in self.bin_cols:
-            self.data[bin_col] = label_encoder.fit_transform(self.data[bin_col])
 
-        self.X_cat = OneHotEncoder(sparse = False).fit_transform(self.data[self.cat_cols].values)
-        self.X_num = StandardScaler().fit_transform(self.data[self.num_cols].values)
-
-        self.X = np.hstack([self.X_num, self.data[self.bin_cols].values, self.X_cat])
-        self.Y = self.data['default'].values
-
-        
     def fill_edu(self, income, edu_income_rel):
         for edu, income_edu in edu_income_rel.items():
             if income_edu[0] < income <  income_edu[1]:
                 return edu
 
 
-
-
-
-
-
-
-
     def return_XY(self):
+        data = self.data.copy()
+        na_index = data[data.education.isna()].index
+        
+        
+        for num_col in self.num_cols:
+            data[num_col] = data[num_col].apply(lambda x: np.log(x + 1))
+
+        label_encoder = LabelEncoder()
+        for bin_col in self.bin_cols:
+            data[bin_col] = label_encoder.fit_transform(data[bin_col])
+        self.X_cat = OneHotEncoder(sparse = False).fit_transform(data[self.cat_cols].values)
+        self.X_num = StandardScaler().fit_transform(data[self.num_cols].values)
+        self.X = np.hstack([self.X_num, data[self.bin_cols].values, self.X_cat])
+        self.Y = data['default'].values
         return self.X, self.Y
-        # return self.data
+
+
+    def return_bal_XY(self, eq=1):
+        data = self.data.copy()
+        n1 = data[data.default==1].shape[0]
+        data_0 = data[data.default==0]
+        data_1 = data[data.default==1]
+        data_balanced = data_0[:n1*eq].append(data_1)
+        X_cat = OneHotEncoder(sparse = False).fit_transform(data_balanced[self.cat_cols].values)
+        X_num = StandardScaler().fit_transform(data_balanced[self.num_cols].values)
+        X = np.hstack([X_num, data_balanced[self.bin_cols].values, X_cat])
+        Y = data_balanced['default'].values
+        return X, Y
+
+    
+
+    def normalize(self):
+        data = self.data.copy()
+
+
+    
--- Projects/Project_4/my_func.py
+++ Projects/Project_4/my_func.py
@@ -23,7 +23,7 @@ def get_boxplot(data, column, target):
     функция строит график типа seaborn.boxplot на основе введенных параметров.
     Принимает объекты dataframe, имя колонки для оси Х имя колонки для оси Y
     """
-    fig, ax = plt.subplots(figsize = (20, 5))
+    fig, ax = plt.subplots(figsize = (10, 5))
     sns.boxplot(x=column, y=target, 
                 data=data,
                ax=ax)
